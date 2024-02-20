9dac53dee948f76637617ad002b68537b2992bb0
jbrennan414 <brennanj414@gmail.com>
2024-02-19 19:33:39 -0700

    stub out cdk stack

---
+++ test/slice-sip.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const SliceSip = require('../lib/slice-sip-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/slice-sip-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new SliceSip.SliceSipStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
